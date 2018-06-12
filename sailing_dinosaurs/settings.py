c50d6f30dc32535a52f041b6ee5ad9e0452e5091
Jeffrey Kam <jeffreykam0415@gmail.com>
2018-06-12 16:11:32 -0400

    Adding Heroku as allowed host

--- sailing_dinosaurs/settings.py
+++ sailing_dinosaurs/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'hm*m-^7l7m3ogup&2tz+kvavp)8rice!30$kh$(8)u5%c)n!d_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sailing-dinosaurs-system.herokuapp.com']
 
 SILENCED_SYSTEM_CHECKS = ["fields.W161"]
 
