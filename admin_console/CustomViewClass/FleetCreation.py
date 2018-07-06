c02afda6c411397ae3939a73f1a25fb99de9a79d
Jeffrey Kam <jeffreykam0415@gmail.com>
2018-07-06 10:40:11 -0400

    added post dicts variables

--- admin_console/CustomViewClass/FleetCreation.py
+++ admin_console/CustomViewClass/FleetCreation.py
@@ -6,27 +6,43 @@ from ..models import *
 
 
 class FleetCreationView(EventCreationView):
-    def _add(self):
-        pass;
-
-    def _edit(self, key):
-        pass;
-
-    def _delete(self, key):
-        pass;
-
     def abstractFormProcess(self, action, **kwargs):
-        self.post_dict = dict(self.request.POST);
+        def add():
+            event_creation = self.base_class();
+
+        def edit(key):
+            event_creation = self.base_class.objects.get(id=key);
+            pass;
+
+        def delete(key):
+            event_creation = self.base_class.objects.get(id=key);
+            pass;
+
+        post_dict = dict(self.request.POST);
+
+        event_type = getPostObj(self.post_dict, 'event_creation_event_type');
+        event_name = getPostObj(self.post_dict, 'event_creation_event_name');
+        event_status = getPostObj(self.post_dict, 'event_creation_event_status');
+        event_description = getPostObj(self.post_dict, 'event_creation_event_description');
+        event_location = getPostObj(self.post_dict, 'event_creation_event_location');
+        event_region = getPostObj(self.post_dict, 'event_creation_event_region');
+        event_host = getPostObj(self.post_dict, 'event_creation_event_host');
+        event_team = getPostObj(self.post_dict, 'event_creation_event_team');
+        event_num_race = getPostObj(self.post_dict, 'event_creation_event_num_race');
+        event_num_boat = getPostObj(self.post_dict, 'event_creation_event_num_boat');
+        event_start_date = getPostObj(self.post_dict, 'event_creation_event_start_date');
+        event_end_date = getPostObj(self.post_dict, 'event_creation_event_end_date');
+
         try:
             dispatcher = super().populateDispatcher();
             if dispatcher.get(action):
                 event_creation_id = kwargs.pop('id', None);
                 if action == 'edit':
-                    self._edit(event_creation_id);
+                    edit(event_creation_id);
                 elif action == 'delete':
-                    self._delete(event_creation_id);
+                    delete(event_creation_id);
             else:
                 if action == 'add':
-                    self._add();
+                    add();
         except:
             print({"Error": "Cannot Process " + action.title() + " Request." });
