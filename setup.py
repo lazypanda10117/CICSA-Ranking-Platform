ddd1c48430ccc525b7ae3d7ace73c3ab767f1e8f
Jeffrey Kam <jeffreykam0415@gmail.com>
2018-07-16 02:04:20 -0400

    Finally fixed the setup script

--- setup.py
+++ setup.py
@@ -3,37 +3,39 @@ import psycopg2
 from sqlalchemy.engine.url import make_url
 
 
-def executeScriptsFromFile(filename, cursor):
+def executeScriptsFromFile(filename, cu):
     fd = open(filename, 'r');
     sql_file = fd.read();
     fd.close();
     sql_commands = sql_file.split(';');
     for command in sql_commands:
         try:
-            print(command);
-            cursor.execute(command);
+            if not command == '':
+                cu.execute(command)
+            else:
+                continue;
         except psycopg2.OperationalError as msg:
             print("Command skipped: " + msg);
 
 
-if os.environ.get('SETUP_STATE') == 'TRUE':
-    db_remote_url = None;
-    db_url = 'localhost';
-    db_name = 'ranking';
-    db_user = 'lazypanda';
-    db_pwd = '';
-    db_port = '5432';
+db_remote_url = None;
+db_url = 'localhost';
+db_name = 'ranking';
+db_user = 'lazypanda';
+db_pwd = '';
+db_port = '5432';
 
-    if os.environ.get('DATABASE_URL') is not None:
-        db_remote_url = make_url(os.environ.get('DATABASE_URL'));
-        db_url = db_remote_url.host;
-        db_name = db_remote_url.database;
-        db_user = db_remote_url.username;
-        db_pwd = db_remote_url.password;
-        db_port = db_remote_url.port;
+if os.environ.get('DATABASE_URL') is not None:
+    db_remote_url = make_url(os.environ.get('DATABASE_URL'));
+    db_url = db_remote_url.host;
+    db_name = db_remote_url.database;
+    db_user = db_remote_url.username;
+    db_pwd = db_remote_url.password;
+    db_port = db_remote_url.port;
 
-    connection_string = "dbname=%s user=%s password=%s host=%s port=%s" % (db_name, db_user, db_pwd, db_url, db_port)
-    conn = psycopg2.connect(connection_string);
-    cursor = conn.cursor();
-    executeScriptsFromFile('setup.sql', cursor);
-    conn.close();
+connection_string = "dbname=%s user=%s password=%s host=%s port=%s" % (db_name, db_user, db_pwd, db_url, db_port)
+conn = psycopg2.connect(connection_string);
+conn.autocommit = True;
+cursor = conn.cursor();
+executeScriptsFromFile('setup.sql', cursor);
+conn.close();
