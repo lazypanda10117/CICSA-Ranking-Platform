a42cec52fec47991b19faeadccfe0ae1425d7cce
Jeffrey Kam <jeffreykam0415@gmail.com>
2018-08-12 17:46:21 -0400

    fixing a bit of API

--- blackbox/api/API.py
+++ blackbox/api/API.py
@@ -1,9 +1,9 @@
 from abc import ABC, abstractmethod
-from ..generalFunctions import *
+from misc.GeneralFunctions import generalFunctions as gf
 
 class API(ABC):
     def __init__(self, request):
         self.request = request;
 
     def authenitication(self):
-        return signed_in(self.request, 'admin');
+        return gf.signed_in(self.request, 'admin');
--- blackbox/api/EventActivityAPI.py
+++ blackbox/api/EventActivityAPI.py
@@ -1,40 +1,39 @@
-import json
-from .API import *
-from ..generalFunctions import *
-from ..models import *
+from .API import API
+from misc.GeneralFunctions import generalFunctions as gf
+from cicsa_ranking.models import EventActivity, MemberGroup,EventTeam, Team
 
 class EventActivityAPI(API):
     def getEventActivity(self, **kwargs):
         return getModelObject(EventActivity, **kwargs);
 
     def updateEventActivityState(self, event_activity_id, event_activity_status):
-        event_activity = getModelObject(EventActivity, id=event_activity_id);
+        event_activity = gf.getModelObject(EventActivity, id=event_activity_id);
         event_activity.event_activity_status = event_activity_status;
         event_activity.save();
 
     def updateEventActivityResult(self, event_activity_id, event_activity_result):
-        event_activity = getModelObject(EventActivity, id=event_activity_id);
+        event_activity = gf.getModelObject(EventActivity, id=event_activity_id);
         event_activity.event_activity_result = event_activity_result;
         event_activity.save();
 
     def getMemberGroupName(self, member_group_id):
         if member_group_id is not None:
-            return getModelObject(MemberGroup, id=member_group_id).member_group_name;
+            return gf.getModelObject(MemberGroup, id=member_group_id).member_group_name;
         return 'Unlinked';
 
     def getMemberGroupLink(self, member_group_id):
         if member_group_id is not None:
-            return getModifiyLink('member group', id=member_group_id);
+            return gf.getModifiyLink('member group', id=member_group_id);
         return '#';
 
     def getEventTeamLinks(self, **kwargs):
-        return filterModelObject(EventTeam, **kwargs);
+        return gf.filterModelObject(EventTeam, **kwargs);
 
     def getEventTeam(self, **kwargs):
-        return getModelObject(Team, **kwargs);
+        return gf.getModelObject(Team, **kwargs);
 
     def getEventActivityModifyLink(self, **kwargs):
-        return getModifiyLink('event activity', **kwargs);
+        return gf.getModifiyLink('event activity', **kwargs);
 
     def getEventTeamModifyLink(self, **kwargs):
-        return getModifiyLink('event team', **kwargs);
+        return gf.getModifiyLink('event team', **kwargs);
