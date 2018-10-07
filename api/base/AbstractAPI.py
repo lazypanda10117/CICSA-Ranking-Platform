5a04d5ed786a2d0c91984731595d90d83a24b9d6
Jeffrey Kam <jeffreykam0415@gmail.com>
2018-10-07 06:28:27 -0400

    just small changes on abstractapi

--- api/base/AbstractAPI.py
+++ api/base/AbstractAPI.py
@@ -1,9 +1,8 @@
 from abc import ABC
-from misc.CustomFunctions import MiscFunctions, RequestFunctions
+from misc.CustomFunctions import RequestFunctions
 from ..authentication.AuthenticationFactory import AuthenticationFactory
 
 
-# not much use of a class right now, but might be more useful later
 class AbstractAPI(ABC):
     def __init__(self, request):
         self.request = request
