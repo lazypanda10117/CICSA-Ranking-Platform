bb1137084a5cd20dc5a7c35361e3acc7f20dd662
Jeffrey Kam <jeffreykam0415@gmail.com>
2018-10-07 06:08:06 -0400

    fix index client redirection

--- client/urls.py
+++ client/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index', views.index, name='client.index'),
+    path('', views.index, name='client.index'),
     path('scoring/<int:id>', views.scoring, name='client.scoring'),
     path('regattas', views.regattas, name='client.regattas')
 ]
