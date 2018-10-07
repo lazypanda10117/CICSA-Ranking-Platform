435aa5809d4234546f9c569d28f9bdda0a1414dd
Jeffrey Kam <jeffreykam0415@gmail.com>
2018-10-07 06:19:14 -0400

    fixing prev authentication fix

--- api/base/AbstractAPI.py
+++ api/base/AbstractAPI.py
@@ -7,5 +7,5 @@ from ..authentication.AuthenticationFactory import AuthenticationFactory
 class AbstractAPI(ABC):
     def __init__(self, request):
         self.request = request
-        utype = self.request.session['utype'] if RequestFunctions.sessionChecker(self.request, ['utype']) else None
+        utype = self.request.session['utype'] if RequestFunctions.sessionChecker(self.request, 'utype') else None
         self.auth = AuthenticationFactory(utype).dispatch()
