435527db715ce0305cb7bd81667442bcf2ec5121
Jeffrey Kam <jeffreykam0415@gmail.com>
2018-10-16 15:43:20 -0400

    throwing error as link broken in data management panel

--- panel/component/CustomElements/CustomElement.py
+++ panel/component/CustomElements/CustomElement.py
@@ -170,4 +170,7 @@ class DBMap:
     @staticmethod
     # NOTE: potential error if use API, so will leave this as is right now
     def getMap(query_class, query_id, target_field):
-        return MiscFunctions.noneCatcher(target_field, ModelFunctions.getModelObject(query_class, id=query_id).__dict__)
+        result =  ModelFunctions.getModelObject(query_class, id=query_id)
+        if result is not None:
+            return MiscFunctions.noneCatcher(target_field, result.__dict__)
+        return 'Link Broken'
