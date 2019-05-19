5aee25c0bf76cebce50c25a06e8b5b8026f38f7e
Jeffrey Kam <jeffreykam0415@gmail.com>
2019-05-19 02:34:19 -0700

    Added news api structure

--- api/functional_api/NewsAPI.py
+++ api/functional_api/NewsAPI.py
@@ -4,4 +4,31 @@ from ..base.AbstractAPI import AbstractAPI
 
 
 class NewsAPI(AbstractAPI):
+    def getRecentNewsPreviews(self):
+        pass
+
+    def getRecentNews(self, size=5):
+        pass
+
+    def getNews(self):
+        pass
+
+    def archiveNews(self):
+        pass
+
+    def addNews(self):
+        pass
+
+    def deleteNews(self):
+        pass
+
+    def getComments(self):
+        pass
+
+    def addComment(self):
+        pass
+
+    def replyComment(self):
+        pass
+
     pass
