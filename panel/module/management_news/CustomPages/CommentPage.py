1538002b7bf0cc408cc7cf9a2fe4a01672bd6a93
Jeffrey Kam <jeffreykam0415@gmail.com>
2019-05-23 04:51:02 +0000

    Finishing up backend code for generating the page objects for comment and news

--- panel/module/management_news/CustomPages/CommentPage.py
+++ panel/module/management_news/CustomPages/CommentPage.py
@@ -1,35 +1,33 @@
-from api import SchoolAPI, EventTeamAPI, TeamAPI, MemberGroupAPI
 from panel.module.base.block.CustomPages import AbstractBasePage
 from panel.module.base.block.CustomComponents import BlockObject, BlockSet, PageObject
+from api.functional_api import NewsAPI
 
 
-class CommentPage(AbstractBasePage):
+class PostPage(AbstractBasePage):
+    def getPagePath(self):
+        return 'platform/module/management_news/admin_comment.html'
+
     def generateList(self):
-        def genEventTeamDict(activity_id):
-            event_teams = event_team_api.filterSelf(event_team_event_activity_id=activity_id)
-            event_team_dict = map(lambda event_team: dict(
-                element_text=(lambda x: school_api.getSelf(id=x.team_school).school_name + ' - ' + x.team_name)
-                (team_api.getSelf(id=event_team.event_team_id)),
-                element_link=event_team_api.getEventTeamModifyLink(id=event_team.id),
-                elements=[
-                    dict(
-                        text=member_group_api.getMemberGroupName(event_team.event_team_member_group_id),
-                        link=member_group_api.getMemberGroupLink(event_team.event_team_member_group_id)
-                    )
-                ]
-            ),
-                             [event_team for event_team in event_teams])
-            return BlockObject('Event Teams', 'Event Team', ['Member Group Link'], event_team_dict)
+        def genCommentDict(news_id):
+            comments = news_api.getComments(news_id).order_by('-news_post_create_time')
+            comment_dict = map(lambda comment: dict(
+                comment_id=comment.id,
+                # should get the name here
+                comment_owner=comment.news_comment_owner,
+                # should get the title here
+                comment_post_id=comment.news_comment_post_id,
+                # should truncate content for preview
+                comment_content=comment.news_comment_content,
+                comment_create_time=comment.news_comment_create_time
+            ), [comment for comment in comments])
+            return BlockObject('Comments', 'Comment', [], comment_dict)
 
-        event_activity_id = int(self.param["id"])
-        school_api = SchoolAPI(self.request)
-        event_team_api = EventTeamAPI(self.request)
-        team_api = TeamAPI(self.request)
-        member_group_api = MemberGroupAPI(self.request)
-        return BlockSet().makeBlockSet(genEventTeamDict(event_activity_id))
+        news_id = self.param.get("id", None)
+        news_api = NewsAPI(self.request)
+        return BlockSet().makeBlockSet(genCommentDict(news_id))
 
     def render(self):
-        return super().renderHelper(PageObject('Event Activity Related Objects List', self.generateList(), []))
+        return super().renderHelper(PageObject('Comment Management', self.generateList(), []))
 
     def parseParams(self, param):
         match = super().parseMatch('\d+')
--- panel/module/management_news/CustomPages/PostPage.py
+++ panel/module/management_news/CustomPages/PostPage.py
@@ -1,37 +1,31 @@
-from api import SchoolAPI, EventTeamAPI, TeamAPI, MemberGroupAPI
 from panel.module.base.block.CustomPages import AbstractBasePage
 from panel.module.base.block.CustomComponents import BlockObject, BlockSet, PageObject
+from api.functional_api import NewsAPI
 
 
 class PostPage(AbstractBasePage):
-    def generateList(self):
-        def genEventTeamDict(activity_id):
-            event_teams = event_team_api.filterSelf(event_team_event_activity_id=activity_id)
-            event_team_dict = map(lambda event_team: dict(
-                element_text=(lambda x: school_api.getSelf(id=x.team_school).school_name + ' - ' + x.team_name)
-                (team_api.getSelf(id=event_team.event_team_id)),
-                element_link=event_team_api.getEventTeamModifyLink(id=event_team.id),
-                elements=[
-                    dict(
-                        text=member_group_api.getMemberGroupName(event_team.event_team_member_group_id),
-                        link=member_group_api.getMemberGroupLink(event_team.event_team_member_group_id)
-                    )
-                ]
-            ),
-                             [event_team for event_team in event_teams])
-            return BlockObject('Event Teams', 'Event Team', ['Member Group Link'], event_team_dict)
+    def getPagePath(self):
+        return 'platform/module/management_news/admin_post.html'
 
-        event_activity_id = int(self.param["id"])
-        school_api = SchoolAPI(self.request)
-        event_team_api = EventTeamAPI(self.request)
-        team_api = TeamAPI(self.request)
-        member_group_api = MemberGroupAPI(self.request)
-        return BlockSet().makeBlockSet(genEventTeamDict(event_activity_id))
+    def generateList(self):
+        def genPostDict():
+            news_posts = news_api.getAllNews().order_by('-news_post_create_time')
+            news_post_dict = map(lambda post: dict(
+                news_post_id=post.id,
+                news_post_title=post.news_post_title,
+                news_post_content=post.news_post_content,
+                news_post_bumps=post.news_post_bumps,
+                news_post_created_time=post.news_post_created_time
+            ), [post for post in news_posts])
+            return BlockObject('Posts', 'Post', [], news_post_dict)
+
+        news_api = NewsAPI(self.request)
+        return BlockSet().makeBlockSet(genPostDict())
 
     def render(self):
-        return super().renderHelper(PageObject('Event Activity Related Objects List', self.generateList(), []))
+        return super().renderHelper(PageObject('Post Management', self.generateList(), []))
 
     def parseParams(self, param):
-        match = super().parseMatch('\d+')
-        param = dict(id=param)
+        match = super().parseMatch('')
+        param = None
         return param
