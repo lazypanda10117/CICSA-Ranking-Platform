c1329ce4fba160fc33555501ffe995ebcb640c03
Jeffrey Kam <jeffreykam0415@gmail.com>
2019-05-25 00:04:15 -0700

    1st version of news ui working

--- api/client_api/page_api/SpecificNewsPageAPI.py
+++ api/client_api/page_api/SpecificNewsPageAPI.py
@@ -6,23 +6,35 @@ from api.functional_api import NewsAPI
 
 class SpecificNewsPageAPI(GeneralClientAPI):
     def grabPageData(self, **kwargs):
-        def genNewsTable(news_status):
-            news_partition = NewsAPI(self.request).getNews(
-                dict(news_post_status=news_status)
-                ).order_by('-news_post_create_time')
-            news_dict = list(map(lambda news: dict(
+        def genComments(news_id):
+            comments = news_api.getCommentsById(news_id)
+            comments_info = list(map(lambda comment: dict(
+                news_comment_owner=comment.news_comment_owner,
+                news_comment_owner_name="Dummy Comment Poster",
+                news_comment_content=comment.news_comment_content,
+                news_comment_create_time=comment.news_comment_create_time
+            ), list(comments)))
+            return comments_info
+
+        def genNewsTable(news_id):
+            news = news_api.getNewsById(news_id)
+            specific_news_info = list(map(lambda news: dict(
+                news_id=news.id,
                 news_post_title=news.news_post_title,
                 news_post_content=news.news_post_content,
                 news_post_owner=news.news_post_owner,
+                news_post_owner_name="Dummy Owner",
                 news_post_create_time=news.news_post_create_time,
-                news_post_bumps=news.news_post_bumps
-            ), list(news_partition)))
-            return news_dict
-        page_data =  dict(
-            Pinned=genNewsTable(NewsPost.NEWS_POST_PINNED),
-            Active=genNewsTable(NewsPost.NEWS_POST_ACTIVE),
-            Archived=genNewsTable(NewsPost.NEWS_POST_ARCHIVED)
-            )
-        print(page_data)
+                news_post_bumps=news.news_post_bumps,
+                comments=genComments(news_id)
+            ), list(news)))
+            return specific_news_info
+
+        news_id = kwargs.get("id")
+        news_api = NewsAPI(self.request)
+        page_data = dict(
+            authenticated=self.context.authType in ["admin", "team"],
+            specific_news=genNewsTable(news_id)
+        )
         return page_data
 
