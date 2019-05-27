792061d23318648e66c9de17799b05d851305a5d
Jeffrey Kam <jeffreykam0415@gmail.com>
2019-05-26 17:19:26 -0700

    Adding region rule for calculating average score into league scoring function

--- api/functional_api/LeagueScoringAPI.py
+++ api/functional_api/LeagueScoringAPI.py
@@ -1,3 +1,5 @@
+import math
+
 from cicsa_ranking.models import Event
 from api.base import AbstractCoreAPI
 from api.model_api import SchoolAPI, EventAPI, SummaryAPI, ConfigAPI
@@ -10,35 +12,47 @@ class LeagueScoringAPI(AbstractCoreAPI):
     def getCurrentLeagueScoreBySchool(self, school_id):
         current_configuration = ConfigAPI(self.request).getAll()[0]
         current_season = current_configuration.config_current_season
-        events = SchoolAPI(self.request).getParticipatedEvents(school_id, Event.EVENT_STATUS_DONE, current_season)
+        school = SchoolAPI(self.request).getSelf(id=school_id)
+        events = SchoolAPI(self.request).getParticipatedNormalEvents(school_id, Event.EVENT_STATUS_DONE, current_season)
         # TODO: Optimize the current n x m queries ;_;
-        scores = sorted([self.getScoreForEventBySchool(event.id, school_id) for event in events], reverse=True)
-        average_score = self.getAverageScore(scores)
-        final_race_score = self.getFinalRaceScore(school_id, current_season)
+        scores = sorted([self.getScoreForEventBySchool(event, school) for event in events], reverse=True)
+        average_score = self.getAverageScore(scores, school.school_region)
+        final_race_score = self.getFinalRaceScore(school, current_season)
         total_score = average_score + final_race_score
         return total_score
 
-    def getScoreForEventBySchool(self, event_id, school_id):
-        event = EventAPI(self.request).getSelf(id=event_id)
-        position = SummaryAPI(self.request).getSummaryRankingBySchool(event.id, school_id)
+    def getScoreForEventBySchool(self, event, school):
+        position = SummaryAPI(self.request).getSummaryRankingBySchool(event.id, school.id)
         return self.getScoreForEvent(position, event.event_team_number, event.event_class)
 
     def getScoreForEvent(self, position, team_number, event_class):
         # A multivariate function that takes in 3 variables and return the score
         return 0
 
-    def getAverageScore(self, scores):
+    def getAverageScore(self, scores, region):
+        num_school_in_region = SchoolAPI(self.request).filterSelf(school_region=region).count()
+        num_race = len(scores)
+
+        if num_school_in_region in [1, 2]:
+            base_average_race_num = 1
+        elif num_school_in_region >= 3:
+            base_average_race_num = 3
+        else:
+            raise Exception("Try to retrieve a region with less than or equal to 0 school.")
+
+        average_factor = max(base_average_race_num, min(math.ceil(num_race/2), 4))
+
         total_score = 0
         for count, score in enumerate(scores):
-            if count < self.LEAGUE_SCORE_AVERAGE_COUNT:
+            if count < average_factor:
                 total_score += score
             else:
                 break
-        return total_score/self.LEAGUE_SCORE_AVERAGE_COUNT
+        return total_score/average_factor
 
-    def getFinalRaceScore(self, school_id, season):
+    def getFinalRaceScore(self, school, season):
         final_event = EventAPI(self.request).getSelf(event_name__in=Event.EVENT_NAME_FINAL_RACE, event_season=season)
-        final_ranking = SummaryAPI(self.request).getSummaryRankingBySchool(final_event.id, school_id)
+        final_ranking = SummaryAPI(self.request).getSummaryRankingBySchool(final_event.id, school.id)
         if final_event and final_event.event_status == Event.EVENT_STATUS_DONE:
             return self.getScoreForEvent(final_ranking, final_event.event_team_number, final_event.event_class)
         else:
--- api/model_api/SchoolAPI.py
+++ api/model_api/SchoolAPI.py
@@ -8,26 +8,27 @@ class SchoolAPI(GeneralModelAPI):
     def getBaseClass():
         return School
 
-    def getParticipatedEvents(self, school_id, status=None, season=None):
+    def getParticipatedNormalEvents(self, school_id, status=None, season=None):
         if season is None:
             if status is None:
                 return EventAPI(self.request).filterSelf(
                     event_school_ids__contains=[school_id]
-                )
+                ).exclude(event_name__in=Event.EVENT_NAME_FINAL_RACE)
             else:
                 return EventAPI(self.request).filterSelf(
                     event_status=Event.EVENT_STATUS_DONE,
                     event_school_ids_contains=[school_id]
-                )
+                ).exclude(event_name__in=Event.EVENT_NAME_FINAL_RACE)
         else:
             if status is None:
                 return EventAPI(self.request).filterSelf(
                     event_season=season,
                     event_school_ids__contains=[school_id]
-                )
+                ).exclude(event_name__in=Event.EVENT_NAME_FINAL_RACE)
             else:
                 return EventAPI(self.request).filterSelf(
                     event_season=season,
                     event_status=Event.EVENT_STATUS_DONE,
                     event_school_ids_contains=[school_id]
-                )
+                ).exclude(event_name__in=Event.EVENT_NAME_FINAL_RACE)
+
