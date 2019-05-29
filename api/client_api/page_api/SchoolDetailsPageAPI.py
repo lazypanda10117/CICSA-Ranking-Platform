d2cf39b5f2e16ecd8a3de61e60eb021ef07bcdb6
Steven Wong <stevenwong1000777@gmail.com>
2019-05-29 04:41:19 +0000

    Created details part for school details page

--- api/client_api/page_api/SchoolDetailsPageAPI.py
+++ api/client_api/page_api/SchoolDetailsPageAPI.py
@@ -1,16 +1,32 @@
 from api.base.GeneralClientAPI import GeneralClientAPI
+from api.model_api import ConfigAPI, RegionAPI, SchoolAPI
 
 class SchoolDetailsPageAPI(GeneralClientAPI):
     def grabPageData(self, **kwargs):
         def getSchoolInfo(school_id): # or gen?
-            return id # temp
+            # name region status season_score
+            school = school_api.getSelf(id=school_id)
+            region_api = RegionAPI(self.request)
+            region = region_api.getSelf(id=school.school_region)
+            return dict(
+                school_name=school.school_name,
+                school_region=region.region_name,
+                school_status=school.school_status,
+                school_season_score="Twice" # TODO
+            )
 
-        def getSchoolParticipatedEvents(school_id): # or gen?
-            return id # temp
+        def getSchoolParticipatedEvents(school_id, season): # or gen?
+            events = school_api.getParticipatedEvents(school_id, season=season)
+            return events
 
         school_id = kwargs.get("id")
+        school_api = SchoolAPI(self.request)
+
+        current_configuration = ConfigAPI(self.request).getAll()[0]
+        current_season = current_configuration.config_current_season
+
         page_data = dict(
-            school_info = getSchoolInfo(school_id),
-            school_participated_events = getSchoolParticipatedEvents(school_id)
+            school_info=getSchoolInfo(school_id),
+            school_participated_events=getSchoolParticipatedEvents(school_id, current_season)
         )
         return page_data
