290bf5fcfd9cd46ac286c2fad28195d784d33e35
Steven Wong <stevenwong1000777@gmail.com>
2019-05-31 02:37:01 +0000

    Finished season score

--- api/client_api/page_api/SchoolDetailsPageAPI.py
+++ api/client_api/page_api/SchoolDetailsPageAPI.py
@@ -1,23 +1,25 @@
 from django.shortcuts import reverse
 from api.base.GeneralClientAPI import GeneralClientAPI
-from api.model_api import ConfigAPI, RegionAPI, SchoolAPI, SummaryAPI
+from api.model_api import ConfigAPI, RegionAPI, SchoolAPI, ScoreAPI, SummaryAPI
 
 class SchoolDetailsPageAPI(GeneralClientAPI):
     def grabPageData(self, **kwargs):
-        def getSchoolInfo(school_id): # or gen?
+        def getSchoolInfo(school_id, season_id):
             # name region status season_score
             school = school_api.getSelf(id=school_id)
             region_api = RegionAPI(self.request)
             region = region_api.getSelf(id=school.school_region)
+            score_api = ScoreAPI(self.request)
+            season_score = score_api.getSeasonScoreValue(school_id, season_id)
             return dict(
                 school_name=school.school_name,
                 school_region=region.region_name,
                 school_status=school.school_status,
-                school_season_score="Twice" # TODO
+                school_season_score=(season_score if season_score != -1 else "-")
             )
 
-        def getSchoolParticipatedEvents(school_id, season): # or gen?
-            events = school_api.getParticipatedEvents(school_id, season=season)
+        def getSchoolParticipatedEvents(school_id, season_id):
+            events = school_api.getParticipatedEvents(school_id, season=season_id)
             school = school_api.getSelf(id=school_id)
             region_api = RegionAPI(self.request)
             region = region_api.getSelf(id=school.school_region)
@@ -41,7 +43,7 @@ class SchoolDetailsPageAPI(GeneralClientAPI):
         current_season = current_configuration.config_current_season
 
         page_data = dict(
-            school_info=getSchoolInfo(school_id),
+            school_info=getSchoolInfo(school_id, current_season),
             school_participated_events=getSchoolParticipatedEvents(school_id, current_season)
         )
         return page_data
--- api/model_api/ScoreAPI.py
+++ api/model_api/ScoreAPI.py
@@ -5,3 +5,12 @@ class ScoreAPI(GeneralModelAPI):
     @staticmethod
     def getBaseClass():
         return Score
+
+    def getSeasonScoreValue(self, school_id, season_id):
+        try:
+            score = self.getSelf(score_school=school_id, score_season=season_id)
+            if score.score_override_value:
+                return score.score_override_value
+            return score.score_value
+        except Exception:
+            return -1
