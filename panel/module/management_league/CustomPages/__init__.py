cbb8a5fd7f1a8dbbaf2e7b5cada6ffb053c5408b
Jeffrey Kam <jkam@zenefits.com>
2019-06-26 17:49:39 -0700

    Deleting and renaming files for management_league panel app

--- panel/module/management_league/CustomPages/__init__.py
+++ panel/module/management_league/CustomPages/__init__.py
@@ -1,4 +1,2 @@
-from . EventActivityDetailPage import EventActivityDetailPage
-from . EventActivityPage import EventActivityPage
-from . EventPage import EventPage
-from . EventChoicePage import EventChoicePage
+from .LeagueRankingPage import LeagueRankingPage
+from .LeagueScoreCompilePage import LeagueScoreCompilePage
