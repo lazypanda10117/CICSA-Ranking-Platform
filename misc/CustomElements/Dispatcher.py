1b683bd012d49ecc0e510e2c250560dcc8f24dcb
Jeffrey Kam <jkam@zenefits.com>
2019-06-28 17:49:16 -0700

    Removing DS Store and adding error handling in dispatcher

--- misc/CustomElements/Dispatcher.py
+++ misc/CustomElements/Dispatcher.py
@@ -1,15 +1,19 @@
 class Dispatcher:
     def __init__(self):
-        self.dispatchTable = {}
+        self.dispatchTable = dict()
 
     def add(self, link_name, link_obj):
         self.dispatchTable[link_name] = link_obj
 
     def get(self, link_name):
+        if link_name not in self.dispatchTable:
+            raise Exception("Cannot get {} from Dispatcher".format(link_name))
         return self.dispatchTable[link_name]
 
     def update(self, link_name, link_obj):
         self.dispatchTable[link_name] = link_obj
 
     def delete(self, link_name):
+        if link_name not in self.dispatchTable:
+            raise Exception("Cannot delete {} from Dispatcher".format(link_name))
         del self.dispatchTable[link_name]
