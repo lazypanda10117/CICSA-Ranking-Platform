e722c2f4c3a4b1a1a37e34b1041b31d18265bf89
Steven Wong <stevenwong1000777@gmail.com>
2019-07-03 00:49:30 -0400

    Modify verify

--- api/authentication/services/ArchiveService.py
+++ api/authentication/services/ArchiveService.py
@@ -9,13 +9,15 @@ class ArchiveService(BaseService):
         return [o for o in objects if not self.__isArchived(o)]
 
     def _verifyADD(self):
-        return self.__filterArchived(self.objects)
+        # return self.__filterArchived(self.objects)
+        return self.objects
 
     def _verifyEDIT(self):
         return self.__filterArchived(self.objects)
 
     def _verifyDELETE(self):
-        return self.__filterArchived(self.objects)
+        # return self.__filterArchived(self.objects)
+        return self.objects
 
     def _verifyVIEW(self):
         return self.objects
