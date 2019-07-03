bdb651a5b87bafcf35bd78ed836bc383074cd46f
Steven Wong <stevenwong1000777@gmail.com>
2019-07-03 00:51:26 -0400

    Add comments

--- api/authentication/services/ArchiveService.py
+++ api/authentication/services/ArchiveService.py
@@ -8,11 +8,13 @@ class ArchiveService(BaseService):
     def __filterArchived(self, objects):
         return [o for o in objects if not self.__isArchived(o)]
 
+    # Don't need here if default behavior?
     def _verifyADD(self):
         # return self.__filterArchived(self.objects)
         return self.objects
 
     def _verifyEDIT(self):
+        # Don't allow editing of archived objects
         return self.__filterArchived(self.objects)
 
     def _verifyDELETE(self):
