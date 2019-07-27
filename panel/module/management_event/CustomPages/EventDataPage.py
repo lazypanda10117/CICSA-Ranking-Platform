636efe5c1ff07e9aba556c7d16ea99f5ac7ead4a
Jeffrey Kam <jkam@zenefits.com>
2019-07-26 18:05:29 -0700

    Refactoring 8

--- panel/module/management_event/CustomPages/EventDataPage.py
+++ panel/module/management_event/CustomPages/EventDataPage.py
@@ -1,6 +1,17 @@
+from misc.CustomElements import Dispatcher
+from panel.module.ModuleRegistry import ModuleRegistry
 from panel.module.base.structure.data_app.CoreViews import CoreDataView
-
+from panel.module.management_event.CustomComponents import sth
 
 class EventDataPage(CoreDataView):
-    def __init__(self):
+    def _setAppName(self):
+        return ModuleRegistry.MANAGEMENT_EVENT
+
+    def _setViewDispatcher(self):
+        dispatcher = Dispatcher()
+        dispatcher.add('')
+        return dispatcher
+
+    def _getTemplateBase(self):
         pass
+
