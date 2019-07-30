802bbd41537b6a33d5df0f976695b12fdf0c976d
Jeffrey Kam <jkam@zenefits.com>
2019-07-29 18:12:58 -0700

    Refactoring 18 - Fixing up Event Choice Pages

--- panel/module/management_event/CustomPages/EventChoicePage.py
+++ panel/module/management_event/CustomPages/EventChoicePage.py
@@ -1,6 +1,5 @@
 from django.urls import reverse
 
-from api import EventTypeAPI
 from panel.module.base.block.CustomPages import AbstractBasePage
 
 
@@ -9,29 +8,23 @@ class EventChoicePage(AbstractBasePage):
         return 'platform/module/management_event/event.html'
 
     def genPageObject(self):
-        # We are ignoring team racing for active types right now because that is still WIP
-        active_types = EventTypeAPI(self.request).getAll().filter(event_type_name='fleet')
-        wip_types = EventTypeAPI(self.request).getAll().filter(event_type_name='team')
-        contents = list()
-        for event_type in active_types:
-            type_name = event_type.event_type_name
-            contents.append(
-                dict(
-                    event_type=type_name,
-                    destination=reverse(
-                        'panel.module.management_event.view_dispatch_param',
-                        args=['event', type_name]
-                    )
-                )
-            )
-        for event_type in wip_types:
-            type_name = event_type.event_type_name + ' (WIP)'
-            contents.append(
-                dict(
-                    event_type=type_name,
-                    destination='#'
-                )
-            )
+        contents = [
+            dict(
+                event_type='Fleet Race',
+                description='View all the fleet race event related entries here',
+                button_description='View Fleet Races',
+                destination=reverse(
+                    'panel.module.management_event.view_dispatch_param',
+                    args=['event', 'fleet race']
+                ),
+            ),
+            dict(
+                event_type='Team Race (Work in Progress)',
+                description='Still a work in progress',
+                button_description='View Team Races',
+                destination='#',
+            ),
+        ]
         return dict(block_title='Event Types', contents=contents)
 
     def render(self):
