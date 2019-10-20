fd32d94328dc7bb486680ee72e0cfedfab243ac8
Jeffrey Kam <jeffreykam0415@gmail.com>
2019-10-19 23:12:05 -0400

    Finish up the panel page for event team management, initialize the update process

--- panel/module/management_event/CustomProcesses/UpdateParticipatingTeamsProcess.py
+++ panel/module/management_event/CustomProcesses/UpdateParticipatingTeamsProcess.py
@@ -7,7 +7,12 @@ from panel.module.base.block.CustomProcesses import AbstractBaseProcess
 class UpdateParticipatingTeamsProcess(AbstractBaseProcess):
     def process(self):
         post_dict = dict(self.request.POST)
-        print(post_dict)
+        # Post Dict Data Example
+        # {'event_team_management_checkbox_2': ['on'], 'event_team_management_checkbox_4': ['on'],
+        #  'event_team_management_checkbox_5': ['on'], 'event_team_management_checkbox_7': ['on'],
+        #  'event_team_management_checkbox_8': ['on'], 'event_team_management_checkbox_9': ['on'],
+        #  'event_team_management_checkbox_10': ['on'], 'event_team_management_checkbox_13': ['on'],
+        #  'event_team_management_checkbox_14': ['on'], 'event_team_management_checkbox_15': ['on']}
         return redirect(
             reverse(
                 'panel.module.management_event.view_dispatch_param',
